bc4173fa60e3e4e7437756ccbe9772236efaa1aa
DenormeLukas <lukas.denorme@student.howest.be>
2021-02-24 10:00:14 +0100

    error fixed

--- ui.js
+++ ui.js
@@ -3,19 +3,22 @@ const React = require("react");
 const { Text, Box } = require("ink");
 import SelectInput from 'ink-select-input';
 
+
 const App = ({ name = 'Istanbul! ' }) => (
 
-	<Box borderStyle="round" borderColor="red" width="42.5%">
-		<Text color="yellow">
-			{" "}Welkom,{" "}in kebabzaak {name}
-		</Text>
-	</Box>
+
+	<>
+		<Box borderStyle="round" borderColor="red" width="42.5%">
+			<Text color="yellow">
+				{" "}Welkom,{" "}in kebabzaak {name}
+			</Text>
+		</Box>
 
 
-	<Box borderStyle="round" borderColor="red" width="42.5%">
-		<SelectInput items={items} onSelect={handleSelect} />
-	</Box>
-
+		<Box borderStyle="round" borderColor="red" width="42.5%">
+			<SelectInput items={items} onSelect={handleSelect} />
+		</Box>
+	</>
 );
 
 module.exports = App;
