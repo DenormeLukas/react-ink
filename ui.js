4c55f404165490855587b012296c3863b2b01429
DenormeLukas <lukas.denorme@student.howest.be>
2021-02-24 10:05:07 +0100

    choice aparte component

--- ui.js
+++ ui.js
@@ -1,11 +1,10 @@
 "use strict";
 const React = require("react");
 const { Text, Box } = require("ink");
-const SelectInput = require('ink-select-input');
+const Choice = require('components/choice.js');
 
 
 const App = ({ name = 'Istanbul! ' }) => (
-
 
 	<>
 		<Box borderStyle="round" borderColor="red" width="42.5%">
@@ -14,11 +13,10 @@ const App = ({ name = 'Istanbul! ' }) => (
 			</Text>
 		</Box>
 
+		<Choice items={items} />
 
-		<Box borderStyle="round" borderColor="red" width="42.5%">
-			<SelectInput items={items} onSelect={handleSelect} />
-		</Box>
 	</>
+
 );
 
 module.exports = App;
