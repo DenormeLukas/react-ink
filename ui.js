6a5e0907422e0ffcd1e127ff8350f933e8769bed
DenormeLukas <lukas.denorme@student.howest.be>
2021-02-24 11:04:06 +0100

    sl;kdfj

--- ui.js
+++ ui.js
@@ -1,7 +1,6 @@
 "use strict";
 const React = require("react");
 const importJsx = require("import-jsx");
-const { Text, Box } = require("ink");
 const Intro = importJsx('./components/Intro.js');
 const Choice = importJsx('./components/Choice.js');
 
