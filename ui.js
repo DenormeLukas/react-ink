758d30303434115d89ddd4d392508bf3e574b2a3
DenormeLukas <lukas.denorme@student.howest.be>
2021-02-24 10:56:07 +0100

    choice added

--- ui.js
+++ ui.js
@@ -3,16 +3,19 @@ const React = require("react");
 const importJsx = require("import-jsx");
 const { Text, Box } = require("ink");
 const Intro = importJsx('./components/Intro.js');
-//const Choice = importJsx('./components/Choice.js');
+const Choice = importJsx('./components/Choice.js');
 
 
 const App = ({ name = 'Istanbul! ' }) => {
 
 	return (
+		<>
 
+			<Intro name={name} />
 
-		<Intro name={name} />
+			<Choice name={items} />
 
+		</>
 	)
 };
 
